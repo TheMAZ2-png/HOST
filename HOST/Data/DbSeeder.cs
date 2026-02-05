ed95b19af68f8a58f4d46e26ff2481afa36760ed
Mazurek, Christopher <cmazure@jacksonville.edu>
2026-02-05 14:11:45 -0500

    Class update 3FEB2026

---
+++ HOST/Data/DbSeeder.cs
@@ -0,0 +1,40 @@
+﻿using System.Data;
+using Microsoft.AspNetCore.Identity;
+using HOST.Constants;
+
+
+namespace HOST.Data
+{
+    
+    
+    
+    public class DbSeeder
+    {
+    public static async Task SeedRolesAndAdmin(IServiceProvider service)
+        {
+            //Seed Roles
+            var userManager = service.GetService<UserManager<IdentityUser>>();
+            var roleManager = service.GetService<UserManager<IdentityUser>>();
+            await roleManager.CreateAsync(new IdentityRole(Roles.Admin.ToString()));
+            await roleManager.CreateAsync(new IdentityRole(Roles.User.ToString()));
+
+            // Creating Admin
+            var user = new IdentityUser
+            {
+                UserName = "admin@gmail.com",
+                Email = "admin@gmail.com",
+                EmailConfirmed = true,
+                PhoneNumberConfirmed = true
+            };
+            var userInDb = await userManager.FindByEmailAsync(user.Email);
+            if(userInDb == null)
+            {
+                await userManager.CreateAsync(user, "Admin@123");
+                await userManager.AddToRoleAsync(user, Roles.Admin.ToString());
+            }
+        
+        
+        }
+    }
+
+}
