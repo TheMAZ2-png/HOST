e745f08b0a729904f4eaa47d6065786b1e06a660
Mazurek, Christopher <cmazure@jacksonville.edu>
2026-03-30 11:34:48 -0400

    UI and bug fixes.
    Hosts now are able to seat parties, just as Managers have been able to.
    Servers are view only currently, until their workflow is finished (shifts, and using that to connect them to parties and tables).
    UI is now consistent, across all pages (including new pages and features).
    
    -CSM

--- HOST/Pages/RestaurantTables/Edit.cshtml.cs
+++ HOST/Pages/RestaurantTables/Edit.cshtml.cs
@@ -7,7 +7,8 @@ using Microsoft.EntityFrameworkCore;
 
 namespace HOST.Pages.RestaurantTables
 {
-    [Authorize]
+    [Authorize(Roles = "Host,Manager")]
+
     public class EditModel : PageModel
     {
         private readonly ApplicationDbContext _context;
