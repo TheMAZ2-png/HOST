ef28cfed7fa5eace8f7db3da2534f7f6d77812dd
Christopher Mazurek <csm012004@outlook.com>
2026-04-09 13:19:21 -0400

    Insight page overhaul, including all prevalent information for managers.
    -CSM

---
+++ HOST/Pages/AIPage/AIChatPage.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace HOST.Pages.AIPage
+{
+    public class AIChatPageModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
